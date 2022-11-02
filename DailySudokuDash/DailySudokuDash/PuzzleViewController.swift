cfafe1d401e82d50cc57b26d821103cbdc6df9b5
mmbrooks <68748259+mmbrooks@users.noreply.github.com>
2022-11-02 18:33:11 -0500

    Storyboard v1

---
+++ DailySudokuDash/DailySudokuDash/PuzzleViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PuzzleViewController.swift
+//  DailySudokuDash
+//
+//  Created by Madeline Brooks on 11/2/22.
+//
+
+import UIKit
+
+class PuzzleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ DailySudokuDash/DailySudokuDash/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DailySudokuDash
+//
+//  Created by Madeline Brooks on 11/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
